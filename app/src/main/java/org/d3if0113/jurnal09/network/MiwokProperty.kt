8d94e421a641f2a60d06d73b86acef1e48bc11b1
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-03-31 06:55:31 +0700

    pembuatan property miwok

--- app/src/main/java/org/d3if0113/jurnal09/network/MiwokProperty.kt
+++ app/src/main/java/org/d3if0113/jurnal09/network/MiwokProperty.kt
@@ -1,2 +1,7 @@
 package org.d3if0113.jurnal09.network
 
+data class MiwokProperty(
+    val category: String,
+    val background: String,
+    val wordList: List<WordList>
+)
---
+++ app/src/main/java/org/d3if0113/jurnal09/network/WordList.kt
@@ -0,0 +1,7 @@
+package org.d3if0113.jurnal09.network
+
+data class WordList(
+    val defaultWord: String,
+    val miwokWord: String,
+    val image: String
+)
