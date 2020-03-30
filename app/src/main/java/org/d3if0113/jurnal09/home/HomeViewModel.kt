91ea2cc6eecd63f6345954fa11fff511f87bb932
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-03-30 23:26:10 +0700

    pemanggilan web service

--- app/src/main/java/org/d3if0113/jurnal09/home/HomeViewModel.kt
+++ app/src/main/java/org/d3if0113/jurnal09/home/HomeViewModel.kt
@@ -1,2 +1,35 @@
 package org.d3if0113.jurnal09.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.d3if0113.jurnal09.network.MiwokAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class HomeViewModel : ViewModel() {
+    private val _response = MutableLiveData<String>()
+
+    val response: LiveData<String>
+        get() = _response
+
+    init {
+        getMiwokData()
+    }
+
+    private fun getMiwokData() {
+        _response.value = "Set the API Response here!"
+        MiwokAPI.retrofitService.getProperties().enqueue(
+            object : Callback<String> {
+                override fun onFailure(call: Call<String>?, t: Throwable?) {
+                    _response.value = "Gagal: ${t?.message}"
+                }
+
+                override fun onResponse(call: Call<String>?, response: Response<String>?) {
+                    _response.value = response?.body()
+                }
+
+            })
+    }
+}
--- app/src/main/java/org/d3if0113/jurnal09/network/MiwokAPIService.kt
+++ app/src/main/java/org/d3if0113/jurnal09/network/MiwokAPIService.kt
@@ -1,2 +1,26 @@
 package org.d3if0113.jurnal09.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+
+private const val BASE_URL = "http://dif.indraazimi.com"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MiwokAPIService {
+    @GET("miwok.json")
+    fun getProperties():
+            Call<String>
+}
+
+object MiwokAPI {
+    val retrofitService: MiwokAPIService by lazy {
+        retrofit.create(MiwokAPIService::class.java)
+    }
+}
