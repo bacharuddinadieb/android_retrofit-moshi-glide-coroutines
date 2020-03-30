63f1d35e3ffa88128891cf327656a55f12521dd1
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-03-30 22:39:22 +0700

    initial commit

---
+++ app/src/main/java/org/d3if0113/jurnal09/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.d3if0113.jurnal09
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
