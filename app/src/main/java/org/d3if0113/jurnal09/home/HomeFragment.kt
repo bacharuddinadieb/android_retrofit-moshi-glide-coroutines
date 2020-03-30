7b572744906759b7f840d73cb05922d7a01fb846
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-03-30 23:43:20 +0700

    pemanggilan fragment home di mainactivity

--- app/src/main/java/org/d3if0113/jurnal09/home/HomeFragment.kt
+++ app/src/main/java/org/d3if0113/jurnal09/home/HomeFragment.kt
@@ -4,20 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import org.d3if0113.jurnal09.R
+import org.d3if0113.jurnal09.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
 
+    private lateinit var dataBinding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by lazy {
+        ViewModelProviders.of(this).get(HomeViewModel::class.java)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        dataBinding.lifecycleOwner = this
+        dataBinding.viewModel = viewModel
+        return dataBinding.root
     }
 
 }
