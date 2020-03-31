8e3f71695957ffc352d89955da3877365cf79dba
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-03-31 11:16:51 +0700

    penambahan parcelabel miwokproperty dan argument di navigasi

--- app/src/main/java/org/d3if0113/jurnal09/network/MiwokProperty.kt
+++ app/src/main/java/org/d3if0113/jurnal09/network/MiwokProperty.kt
@@ -1,7 +1,11 @@
 package org.d3if0113.jurnal09.network
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class MiwokProperty(
     val category: String,
     val background: String,
     val wordList: List<WordList>
-)
+) : Parcelable
--- app/src/main/java/org/d3if0113/jurnal09/network/WordList.kt
+++ app/src/main/java/org/d3if0113/jurnal09/network/WordList.kt
@@ -1,7 +1,11 @@
 package org.d3if0113.jurnal09.network
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class WordList(
     val defaultWord: String,
     val miwokWord: String,
     val image: String = "kosong"
-)
+) : Parcelable
